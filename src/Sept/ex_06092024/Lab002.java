a6074e42360b9d9be1bd2b89427babc1d3ab7a49
SouravQA <sssouravrock@gmail.com>
2024-09-06 06:54:02 +0530

    First commit

--- src/Sept/ex_06092024/Lab002.java
+++ src/Sept/ex_06092024/Lab002.java
@@ -1,4 +1,13 @@
 package Sept.ex_06092024;
 
+import java.sql.SQLOutput;
+
 public class Lab002 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+    }
+
+
 }
