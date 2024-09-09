aa6729963ed85a71d3fd302d46784de613343241
SouravQA <sssouravrock@gmail.com>
2024-09-09 08:10:35 +0530

    Second day commit

---
+++ src/Sept/ex_09092024/Lab003.java
@@ -0,0 +1,27 @@
+package Sept.ex_09092024;
+
+public class Lab003 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        //Keywords
+        /*Reserved words :- easy to use , predefined meaning
+        orange color in Intellij
+        package, class, access modifier,
+         Control statements Exception handling
+         -- all are in lower case
+        compiler understands keywords
+
+         */
+
+
+
+
+
+
+
+    }
+
+
+
+}
---
+++ src/Sept/ex_09092024/Lab004.java
@@ -0,0 +1,20 @@
+package Sept.ex_09092024;
+
+public class Lab004 {
+
+    public static void main(String[] args) {
+        //Variables and Data Types, Identifiers
+
+
+        //int age = 65;
+        /* age is the identifier
+        int is the data type
+        65 is a literal ir value
+
+         */
+    }
+
+
+
+
+}
