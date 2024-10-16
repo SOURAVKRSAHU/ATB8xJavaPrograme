cae7039031e80a93c005826fa9e51c714b600acb
SouravQA <sssouravrock@gmail.com>
2024-10-16 08:22:52 +0530

    Second day commit

---
+++ src/Sept/ex_16102024/Lab101.java
@@ -0,0 +1,19 @@
+package Sept.ex_16102024;
+
+public class Lab101 {
+
+    public static void main(String[] args) {
+
+        XYZ A = null;
+
+    }
+}
+
+
+class XYZ{
+
+    static {
+
+        System.out.println("hello");
+    }
+}
